a50e1e6b3415569fa0436b14b3eac55789b48ab0
Ehab <1152012166@qq.com>
2025-08-23 12:30:13 +0800

    <a1>BPE: speed up

--- assignment1-basics/cs336_basics/bpe.py
+++ assignment1-basics/cs336_basics/bpe.py
@@ -6,6 +6,7 @@ import time
 from copy import deepcopy
 
 PAT = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
+PAT = re.compile(PAT)
 DEBUG = False
 
 class BPE_Tokenizer(object):
@@ -29,17 +30,16 @@ class BPE_Tokenizer(object):
             vocab[i + 256] = token.encode("utf-8")
         self.vocab = vocab
 
-    def pretokenization_for_one_chunk(self, special_tokens: list[str], chunk_range):
+    def pretokenization_for_one_chunk(self, start, end):
         with open(self.input_path, "rb") as f:
-            start, end = chunk_range
             f.seek(start)
             chunk = f.read(end - start).decode("utf-8", errors="ignore")
             
             # split on special tokens
             counter = Counter()
-            docs = re.split("|".join(special_tokens), chunk)
+            docs = re.split("|".join(re.escape(token) for token in self.special_tokens), chunk)
             for doc in docs:
-                for match in re.finditer(PAT, doc):
+                for match in PAT.finditer(doc):
                     counter[match.group()] += 1
 
         return counter
@@ -49,15 +49,14 @@ class BPE_Tokenizer(object):
             boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
             # parallelize this by sending each start/end pair to a set of processes.
-            special_tokens = [re.escape(token) for token in self.special_tokens]
 
             # counters = []
             # for start, end in zip(boundaries[:-1], boundaries[1:]):
             #     print(start, end)
             #     counters.append(self.pretokenization_for_one_chunk(special_tokens, (start, end)))
             with Pool(processes=num_processes) as pool:
-                ranges = list(zip(boundaries[:-1], boundaries[1:]))
-                counters = pool.starmap(self.pretokenization_for_one_chunk, zip([special_tokens]*num_processes, ranges))
+                ranges = zip(boundaries[:-1], boundaries[1:])
+                counters = pool.starmap(self.pretokenization_for_one_chunk, ranges)
         
         total_counter = Counter()
         for counter in counters:
