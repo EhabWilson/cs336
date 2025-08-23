9bfd445bd7879c219e283ac2e436eba8e2ab7d03
Ehab <1152012166@qq.com>
2025-08-23 19:51:21 +0800

    <a1>Transformer: [pass] RMSNorm

--- assignment1-basics/cs336_basics/models.py
+++ assignment1-basics/cs336_basics/models.py
@@ -15,8 +15,8 @@ class Linear(torch.nn.Module):
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return einsum(self.weight, x, "d_out d_in, ... d_in -> ... d_out")
     
-    def load_state_dict(self, weight: torch.Tensor):
-        self.weight.data.copy_(weight)
+    def load_state_dict(self, weights: torch.Tensor):
+        self.weight.data.copy_(weights)
 
 class Embedding(torch.nn.Module):
     def __init__(self, num_embeddings, embedding_dim, device=None, dtype=None):
@@ -30,5 +30,27 @@ class Embedding(torch.nn.Module):
     def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
         return self.embeds[token_ids]
     
-    def load_state_dict(self, weight: torch.Tensor):
-        self.embeds.data.copy_(weight)
+    def load_state_dict(self, weights: torch.Tensor):
+        self.embeds.data.copy_(weights)
+
+class RMSNorm(torch.nn.Module):
+    def __init__(self, d_model: int, eps: float = 1e-5, device=None, dtype=None):
+        super().__init__()
+
+        self.g = nn.Parameter(
+            torch.empty(d_model, dtype=dtype)
+        ).to(device)
+        nn.init.trunc_normal_(self.g, mean=0, std=1, a=-3., b=3.)
+        self.eps = eps
+        self.d_model = d_model
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        in_type = x.dtype
+        x = x.to(torch.float32)
+
+        rms = torch.sqrt(torch.sum(x**2, dim=-1) / self.d_model + self.eps)
+        rms_norm = einsum(x, self.g, "... d_model, d_model -> ... d_model") / rms[...,None]
+        return rms_norm.to(in_type)
+    
+    def load_state_dict(self, weights: torch.Tensor):
+        self.g.data.copy_(weights)
--- assignment1-basics/tests/adapters.py
+++ assignment1-basics/tests/adapters.py
@@ -10,7 +10,7 @@ import torch
 from torch import Tensor
 
 from cs336_basics.bpe import BPE_Tokenizer
-from cs336_basics.models import Linear, Embedding
+from cs336_basics.models import *
 
 
 def run_linear(
@@ -383,7 +383,9 @@ def run_rmsnorm(
         Float[Tensor,"... d_model"]: Tensor of with the same shape as `in_features` with the output of running
         RMSNorm of the `in_features`.
     """
-    raise NotImplementedError
+    rms_norm = RMSNorm(d_model, eps)
+    rms_norm.load_state_dict(weights)
+    return rms_norm(in_features)
 
 
 def run_silu(in_features: Float[Tensor, " ..."]) -> Float[Tensor, " ..."]:
