0ebcf479c2ee0b674f198090fb39ba5f2ee01aeb
Ehab <1152012166@qq.com>
2025-08-23 14:09:30 +0800

    typo

--- assignment1-basics/cs336_basics/pretokenization_example.py
+++ assignment1-basics/cs336_basics/pretokenization_example.py
@@ -50,13 +50,16 @@ def find_chunk_boundaries(
 
 
 ## Usage
-with open(..., "rb") as f:
-    num_processes = 4
-    boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
+if __name__ == '__main__':
+    
+    with open("/root/autodl-tmp/a1-data/TinyStoriesV2-GPT4-valid.txt", "rb") as f:
+        num_processes = 2
+        boundaries = find_chunk_boundaries(f, num_processes, b"<|endoftext|>")
 
-    # The following is a serial implementation, but you can parallelize this
-    # by sending each start/end pair to a set of processes.
-    for start, end in zip(boundaries[:-1], boundaries[1:]):
-        f.seek(start)
-        chunk = f.read(end - start).decode("utf-8", errors="ignore")
-        # Run pre-tokenization on your chunk and store the counts for each pre-token
+        # The following is a serial implementation, but you can parallelize this
+        # by sending each start/end pair to a set of processes.
+        for start, end in zip(boundaries[:-1], boundaries[1:]):
+            f.seek(start)
+            chunk = f.read(end - start).decode("utf-8", errors="ignore")
+            print(chunk[:30])
+            # Run pre-tokenization on your chunk and store the counts for each pre-token
