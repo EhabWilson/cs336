1f4f5a2b749d2e7f9fdf3cea5cc775f0f383ed05
EhabWilson <caoyh20@163.com>
2025-11-06 19:42:22 +0800

    <a5> part7-2 [pass] microbatch grpo

--- assignment5-alignment/cs336_alignment/grpo.py
+++ assignment5-alignment/cs336_alignment/grpo.py
@@ -79,3 +79,27 @@ def masked_mean(
     dim: int | None = None,
 ) -> torch.Tensor:
     return (tensor * mask).sum(dim=dim) / mask.sum(dim=dim)
+
+def grpo_microbatch_train_step(
+    policy_log_probs: torch.Tensor,
+    response_mask: torch.Tensor,
+    gradient_accumulation_steps: int,
+    loss_type: Literal["no_baseline", "reinforce_with_baseline", "grpo_clip"],
+    raw_rewards: torch.Tensor | None = None,
+    advantages: torch.Tensor | None = None,
+    old_log_probs: torch.Tensor | None = None,
+    cliprange: float | None = None,
+) -> tuple[torch.Tensor, dict[str, torch.Tensor]]:  # loss, metadata
+    loss, metadata = compute_policy_gradient_loss(
+        policy_log_probs,
+        loss_type,
+        raw_rewards,
+        advantages,
+        old_log_probs,
+        cliprange
+    )
+
+    loss = masked_mean(loss, response_mask) / gradient_accumulation_steps
+    loss.backward()
+
+    return loss.detach(), metadata 
